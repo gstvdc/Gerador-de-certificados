01963514bba08a9b59a0de13371c58809f1fb9d7
Gustavo Constante <gustavo.cunha.constante@gmail.com>
2025-11-12 14:06:58 -0300

    Testes de ícones e novos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
+import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    ItemCertificadoComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'GeradorCertificado';
+  exibeNavbar: boolean = true;
 }
